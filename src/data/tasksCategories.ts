f216f46ea1dfe0e589e896fcd0985314f3439f41
AShumenkova <AShumenkova@synergy.ru>
2025-06-16 17:21:36 +0300

    categories fix array

--- src/data/tasksCategories.ts
+++ src/data/tasksCategories.ts
@@ -1,37 +1,42 @@
-export const categories: Record<string, {
-  label: string
-  icon: string
-}> = {
-  work: {
+export const categories = [
+  {
+    id: 'work',
     label: 'Работа',
     icon: new URL('@/assets/icons/plan/work.png', import.meta.url).href,
   },
-  health: {
+  {
+    id: 'health',
     label: 'Здоровье',
     icon: new URL('@/assets/icons/plan/health.png', import.meta.url).href,
   },
-  body: {
+  {
+    id: 'body',
     label: 'Спорт',
     icon: new URL('@/assets/icons/plan/body.png', import.meta.url).href,
   },
-  home: {
+  {
+    id: 'home',
     label: 'Дом / Уют',
     icon: new URL('@/assets/icons/plan/home.png', import.meta.url).href,
   },
-  spirit: {
+  {
+    id: 'spirit',
     label: 'Дух',
     icon: new URL('@/assets/icons/plan/spirit.png', import.meta.url).href,
   },
-  creativity: {
+  {
+    id: 'creativity',
     label: 'Творчество',
     icon: new URL('@/assets/icons/plan/creativity.png', import.meta.url).href,
   },
-  study: {
+  {
+    id: 'study',
     label: 'Изучение нового',
     icon: new URL('@/assets/icons/plan/study.png', import.meta.url).href,
   },
-  free: {
+  {
+    id: 'free',
     label: 'Разное',
     icon: new URL('@/assets/icons/plan/free.png', import.meta.url).href,
-  }
-}
+  },
+]
